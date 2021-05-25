7ff776af21b19a43b7bda88342f40a260fb51d81
Amir Zad <ecareteq.com@gmail.com>
2021-05-25 13:54:58 -0400

    test travis

--- src/services/users.service.spec.js
+++ src/services/users.service.spec.js
@@ -3,5 +3,5 @@ import { userByEmail } from "./users.service";
 test("returns user by email", async () => {
   const user = await userByEmail("amir.zad@contractika.com");
   expect(user.email).toBe("amir.zad@contractika.com");
-  expect(user.firstName).toBe("Amir");
+  expect(user.firstName).toBe("Amir Mohsen");
 });
